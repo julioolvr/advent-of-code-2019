40a39f12654d1ae513163a26060f15943b8a5205
Julio <julio.olvr@gmail.com>
2019-12-02 13:32:02 -0300

    Day 2 Part 2

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,23 +1,58 @@
-program = ARGF.each_line.first.split(',').map(&:to_i)
-program_counter = 0
+class IntcodeComputer
+  OPCODE_ADD = 1
+  OPCODE_MULTIPLY = 2
+  OPCODE_STOP = 99
 
-OPCODE_ADD = 1
-OPCODE_MULTIPLY = 2
-OPCODE_STOP = 99
+  def initialize
+    reset
+  end
 
-until program[program_counter] == OPCODE_STOP
-  case program[program_counter]
-  when OPCODE_ADD
-    result = program[program[program_counter + 1]] + program[program[program_counter + 2]]
-    program[program[program_counter + 3]] = result
-    program_counter += 4
-  when OPCODE_MULTIPLY
-    result = program[program[program_counter + 1]] * program[program[program_counter + 2]]
-    program[program[program_counter + 3]] = result
-    program_counter += 4
-  else
-    raise "Invalid opcode #{program[program_counter]}"
+  def execute(program)
+    until program[@instruction_pointer] == OPCODE_STOP
+      case program[@instruction_pointer]
+      when OPCODE_ADD
+        result = program[program[@instruction_pointer + 1]] + program[program[@instruction_pointer + 2]]
+        program[program[@instruction_pointer + 3]] = result
+        @instruction_pointer += 4
+      when OPCODE_MULTIPLY
+        result = program[program[@instruction_pointer + 1]] * program[program[@instruction_pointer + 2]]
+        program[program[@instruction_pointer + 3]] = result
+        @instruction_pointer += 4
+      else
+        raise "Invalid opcode #{program[@instruction_pointer]}"
+      end
+    end
+
+    program
+  end
+
+  def reset
+    @instruction_pointer = 0
   end
 end
 
-puts program[0]
+program = ARGF.each_line.first.split(',').map(&:to_i)
+
+computer = IntcodeComputer.new
+result = computer.execute(program.clone)
+
+puts "Part 1: #{result.first}"
+
+EXPECTED_OUTPUT = 19_690_720
+
+noun, verb = 100.times do |noun|
+  verb = 100.times.find do |verb|
+    computer.reset
+    test_program = program.clone
+    test_program[1] = noun
+    test_program[2] = verb
+    computer.execute(test_program).first == EXPECTED_OUTPUT
+  end
+
+  break noun, verb unless verb.nil?
+end
+
+puts "Part 2:"
+puts "-  Noun: #{noun}"
+puts "-  Verb: #{verb}"
+puts "-  Result: #{noun * 100 + verb}"

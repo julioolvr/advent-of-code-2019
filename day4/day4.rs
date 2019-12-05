19266d4c00134e10c64f94d2f1af0cf6edb8ded2
Julio <julio.olvr@gmail.com>
2019-12-05 08:30:46 -0300

    Day 4 Part 2

--- day4/day4.rs
+++ day4/day4.rs
@@ -15,5 +15,57 @@ fn all_increasing_digits(number: &Vec<char>) -> bool {
 }
 
 fn has_pair_of_digits(number: &Vec<char>) -> bool {
-    number.windows(2).any(|digits| digits[0] == digits[1])
+    number
+        .into_iter()
+        .enumerate()
+        .skip(1)
+        .scan((number[0], 1), |(last_digit, count), (i, &digit)| {
+            if digit != *last_digit && *count == 2 {
+                return Some(true);
+            }
+
+            if digit == *last_digit && *count == 1 && i == number.len() - 1 {
+                return Some(true);
+            }
+
+            if digit == *last_digit {
+                *count += 1;
+            } else {
+                *last_digit = digit;
+                *count = 1;
+            }
+
+            return Some(false);
+        })
+        .any(|result| result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_number_with_pair_of_digits() {
+        assert!(has_pair_of_digits(&vec!['1', '2', '3', '3', '4']));
+    }
+
+    #[test]
+    fn test_number_with_pair_of_digits_at_beginning() {
+        assert!(has_pair_of_digits(&vec!['1', '1', '2', '3', '4']));
+    }
+
+    #[test]
+    fn test_number_with_pair_of_digits_at_end() {
+        assert!(has_pair_of_digits(&vec!['1', '2', '3', '4', '4']));
+    }
+
+    #[test]
+    fn test_number_without_pair_of_digits() {
+        assert!(!has_pair_of_digits(&vec!['1', '2', '3', '4', '5']));
+    }
+
+    #[test]
+    fn test_number_with_more_than_two_consecutive_digits() {
+        assert!(!has_pair_of_digits(&vec!['1', '2', '2', '2', '3']));
+    }
 }

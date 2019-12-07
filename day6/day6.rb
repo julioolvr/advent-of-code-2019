13ef18f449b76839d43ed4f2a7f2257b858d76bc
Julio <julio.olvr@gmail.com>
2019-12-07 16:03:20 -0300

    Day 6 Part 2

--- day6/day6.rb
+++ day6/day6.rb
@@ -1,17 +1,28 @@
 class Orbiter
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-    @orbiting = []
+  def initialize
+    @orbiting = nil
+    @orbiters = []
   end
 
-  def add_orbit(other)
-    @orbiting << other
+  def set_orbit(other)
+    @orbiting = other
+    other.add_orbiter(self)
+  end
+
+  def add_orbiter(other)
+    @orbiters << other
   end
 
   def count_of_orbits
-    @count_of_orbits ||= @orbiting.map(&:count_of_orbits).sum + @orbiting.size
+    if @orbiting
+      @orbiting.count_of_orbits + 1
+    else
+      0
+    end
+  end
+
+  def possible_transfers
+    @orbiters + [@orbiting].compact
   end
 end
 
@@ -19,10 +30,25 @@ bodies = {}
 
 ARGF.each_line do |line|
   target_name, body_name = line.split(')').map(&:chomp)
-  target = bodies[target_name] || (bodies[target_name] = Orbiter.new(target_name))
-  body = bodies[body_name] || (bodies[body_name] = Orbiter.new(body_name))
-  body.add_orbit(target)
+  target = bodies[target_name] || (bodies[target_name] = Orbiter.new)
+  body = bodies[body_name] || (bodies[body_name] = Orbiter.new)
+  body.set_orbit(target)
 end
 
 result = bodies.values.map(&:count_of_orbits).sum
 puts "Part 1: #{result}"
+
+from = bodies['YOU']
+to = bodies['SAN']
+
+steps = 0
+frontier = from.possible_transfers
+visited = frontier
+
+until frontier.include?(to)
+  frontier = frontier.flat_map(&:possible_transfers) - visited
+  visited += frontier
+  steps += 1
+end
+
+puts "Part 2: #{steps - 1}"

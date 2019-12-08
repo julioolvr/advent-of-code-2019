ad188b9f0101597b8344d6ba4e215e1647a1df84
Julio <julio.olvr@gmail.com>
2019-12-08 10:41:11 -0300

    Day 8 Part 2

--- day8/day8.rb
+++ day8/day8.rb
@@ -4,4 +4,16 @@ HEIGHT = 6
 image = ARGF.each_line.first.chomp.split('').map(&:to_i).each_slice(WIDTH * HEIGHT)
 target_layer = image.min_by { |layer| layer.count(0) }
 result = target_layer.count(1) * target_layer.count(2)
-puts result
+puts "Part 1: #{result}"
+
+BLACK = 0
+TRANSPARENT = 2
+
+decoded_image = image.first.map.with_index do |_, i|
+  image.lazy.map { |layer| layer[i] }.find { |pixel| pixel != TRANSPARENT }
+end
+
+puts "Part 2:"
+decoded_image.each_slice(WIDTH) do |line|
+  puts line.map { |pixel| pixel == BLACK ? "\u25FE" : "\u25FD" }.join('')
+end

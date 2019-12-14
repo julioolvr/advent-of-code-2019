f52196f0e8f111dfa2b5f475201907b5e0aede90
Julio <julio.olvr@gmail.com>
2019-12-14 11:45:33 -0300

    Day 11 Part 2

--- day11/day11.rb
+++ day11/day11.rb
@@ -63,4 +63,42 @@ while execution.alive?
   robot_position += robot_direction
 end
 
-puts panels.keys.count
+puts "Part 1: #{panels.keys.count}"
+
+computer = IntcodeComputer.new
+output, execution = computer.execute(program)
+execution.resume
+
+panels = Hash.new(BLACK)
+robot_position = Vector.new(0, 0)
+robot_direction = Vector.new(0, 1)
+panels[robot_position] = WHITE
+
+while execution.alive?
+  execution.resume(panels[robot_position])
+  to_paint, turn = output.last(2)
+  panels[robot_position] = to_paint
+  robot_direction = case turn
+                    when TURN_LEFT
+                      robot_direction.rotate_left
+                    when TURN_RIGHT
+                      robot_direction.rotate_right
+                    end
+  painted += 1 unless panels.key?(robot_position)
+  robot_position += robot_direction
+end
+
+painted_positions = panels.keys
+left = painted_positions.map { |position| position.x }.min
+right = painted_positions.map { |position| position.x }.max
+top = painted_positions.map { |position| position.y }.max
+bottom = painted_positions.map { |position| position.y }.min
+
+puts "Part 2:"
+(bottom..top).to_a.reverse.each do |y|
+  (left..right).each do |x|
+    print panels[Vector.new(x, y)] == BLACK ? "\u25FE" : "\u25FD"
+  end
+
+  puts
+end

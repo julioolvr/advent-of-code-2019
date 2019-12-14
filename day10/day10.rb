905cc5d98c638a84d9b403a0d0806feccece6c6f
Julio <julio.olvr@gmail.com>
2019-12-14 11:08:15 -0300

    Day 10 Part 2

--- day10/day10.rb
+++ day10/day10.rb
@@ -16,27 +16,26 @@ Vector = Struct.new(:x, :y) do
   def angle(other)
     Math.acos(dot(other) / (length * other.length))
   end
+
+  def -(other)
+    Vector.new(x - other.x, y - other.y)
+  end
 end
 
-HORIZONTAL = Vector.new(1, 0)
+VERTICAL = Vector.new(0, -1)
 
-def visible_asteroids_from(x, y, asteroids_map)
-  distances_to_others(x, y, asteroids_map)
-    .map do |distance|
-      angle = distance.angle(HORIZONTAL)
-      angle = 2 * Math::PI - angle if distance[1].negative?
-      angle
-    end
-    .uniq { |angle| angle.round(10) } # Rounding to avoid precision errors, there has to be a better
-                                      # way to handle this...
+def visible_asteroids_from(from, asteroids_map)
+  distances_to_others(from, asteroids_map)
+    .map { |distance| angle_to_vertical(distance) }
+    .uniq
 end
 
-def distances_to_others(x, y, asteroids_map)
+def distances_to_others(from, asteroids_map)
   height = asteroids_map.size
   width = asteroids_map.first.size
 
   all_asteroids(asteroids_map)
-    .map { |(other_x, other_y)| Vector.new(other_x - x, other_y - y) }
+    .map { |other| other - from }
     .sort_by(&:length)
     .drop(1)
 end
@@ -47,10 +46,41 @@ def all_asteroids(asteroids_map)
 
   (0...width).to_a.product((0...height).to_a)
     .select { |(x, y)| asteroids_map[y][x] }
+    .map { |(x, y)| Vector.new(x, y) }
 end
 
-result = all_asteroids(asteroids_map)
-  .map { |(x, y)| visible_asteroids_from(x, y, asteroids_map).count }
-  .max
+def angle_to_vertical(vector)
+  angle = vector.angle(VERTICAL).round(10)
+  angle = 2 * Math::PI - angle if vector.x.negative?
+  angle
+end
 
-puts result
+best_coordinates = all_asteroids(asteroids_map).max_by { |coordinates| visible_asteroids_from(coordinates, asteroids_map).count }
+
+result = visible_asteroids_from(best_coordinates, asteroids_map).count
+
+puts "Part 1: #{result}"
+
+# Create an array of arrays, where each internal array has a list of tuples
+# of [distance_vector, angle_to_vertical]. Each internal array are all the
+# asteroids that share the same angle_to_vertical. Sort the array of arrays
+# so that the ones with the smaller angle come first.
+distances_by_angle = distances_to_others(best_coordinates, asteroids_map)
+  .map { |distance| [distance, angle_to_vertical(distance)] }
+  .group_by { |(_, angle)| angle }
+  .values
+  .sort_by { |distances_and_angles| distances_and_angles.first.last }
+
+# Zip all arrays together (and flatten and compact that) so that we end up
+# with an array of [distance_vector, angle_to_vertical] tuples which is like
+# taking the first element of the array with the smallest angle, the first
+# of the next smallest angle, and so on until we come back all the way around.
+most_asteroids_in_a_row = distances_by_angle.map(&:size).max
+zip_base = [nil] * most_asteroids_in_a_row
+result = zip_base.zip(*distances_by_angle).flatten(1).compact.drop(199).first.first
+
+# Convert the distance vector back to absolute coordinates
+x = best_coordinates.x + result.x
+y = best_coordinates.y + result.y
+
+puts "Part 2: x=#{x} y=#{y} result=#{x * 100 + y}"
